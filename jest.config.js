8e1e5c71950e854b59dd94a6dc0385d290bd701f
xuyuanxiang@home <xuyuanxiang@shouqianba.com>
2019-12-30 22:59:48 +0800

    first commit

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx', 'node'],
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/', '<rootDir>/example/'],
-  transformIgnorePatterns: ['/node_modules/', '<rootDir>/lib/', '<rootDir>/example/'],
-};
--- src/__tests__/index.spec.ts
+++
@@ -1,5 +0,0 @@
-import { foo } from '../index';
-
-it('should work', function() {
-  expect(foo()).toBe('body {\n' + '  margin: 0.32rem;\n' + '}');
-});
