c5a885ee274fda5866dd0ad7f70ebbbd3b4a5138
徐远翔 <xuyuanxiang@wosai-inc.com>
2019-12-31 18:58:39 +0800

    stash

--- src/replace.ts
+++ src/replace.ts
@@ -9,10 +9,7 @@ interface IResultReducer {
 interface IStateMachineHandler {
   next?: (ch: string) => IResultReducer | void;
   flush?: (last?: boolean) => IResultReducer | void;
-}
-class StateMachine {
-  px: IStateMachineHandler = {};
-  [':']: IStateMachineHandler = {};
+  [key: string]: any;
 }
 type State = keyof StateMachine;
 
@@ -24,30 +21,68 @@ function isNumeric(ch: string): boolean {
   return /\d/.test(ch);
 }
 
-export const replace = memoize(10)(function(cssText: string): string {
+class StateMachine {
+  private numeric: string = '';
+  private raw: string = '';
+
+  px: IStateMachineHandler = {
+    next: (ch: string) => {
+      if (isNumeric(ch)) {
+        this.numeric += ch;
+      } else if (isWhitespace(ch)) {
+        if (!this.numeric) {
+          if (this.raw) {
+            const emit = this.raw;
+            this.raw = '';
+            return { emit, skipEmit: true };
+          }
+        } else {
+          const emit = replace(this.numeric);
+          this.numeric = '';
+          this.raw = '';
+          return { emit: replace(this.numeric), skipEmit: true };
+        }
+      }
+      this.raw += ch;
+    },
+  };
+  [':']: IStateMachineHandler = {
+    next: (ch: string) => {
+      if (ch === ':') {
+        this.raw = '';
+        this.numeric = '';
+        return { state: 'px' };
+      }
+    },
+  };
+}
+
+export const replace = memoize(10)(function(cssText: string, last?: boolean): string {
   const stateMachine = new StateMachine();
   let state: State = ':';
   let replaced: string = '';
   const len = cssText.length;
   let pos = 0;
 
-  function apply(reducerResult: IResultReducer, ch?: string): void {
+  function apply(reducerResult?: IResultReducer, ch?: string): void {
     if (!reducerResult) {
-      if (ch) {
-        replaced += ch;
-      }
+      if (ch) replaced += ch;
     } else {
-      if (typeof reducerResult.state !== 'undefined') state = reducerResult.state;
-      if (reducerResult.skipEmit) {
-      }
+      if (reducerResult.state) state = reducerResult.state;
+      if (reducerResult.emit) replaced += reducerResult.emit;
+      else if (reducerResult.skipEmit !== true && ch !== undefined) replaced += ch;
     }
   }
+
   while (pos < len) {
     const ch = cssText[pos++];
+    const prevState = state;
     const handler = stateMachine[state];
     const reducerResult = handler.next && handler?.next(ch);
     if (reducerResult) {
+      console.log('before apply(', { ch, state: prevState, replaced }, ')');
       apply(reducerResult, ch);
+      console.log('after apply(', { reducerResult, state, replaced }, ')');
     }
     // switch (state) {
     //   case ':':
@@ -82,7 +117,7 @@ export const replace = memoize(10)(function(cssText: string): string {
   }
   const handler = stateMachine[state];
   if (handler && handler.flush) {
-    const reducerResult = handler.flush(true);
+    const reducerResult = handler.flush(last);
     if (reducerResult) {
       apply(reducerResult);
     }
